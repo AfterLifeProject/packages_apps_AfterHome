08b5684074ba9fe5c62e260c9501d5aff72c240f
SagarMakhar <sagarmakhar@gmail.com>
2021-08-27 16:50:06 +0000

    Launcher3: Allow locking tasks to recents [2/2]
    - Drawables are from similar commit of oreo times
    
    Change-Id: Ib444ab50eeeb4fafdf0492edef5bed471fa1753a
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- quickstep/src/com/android/quickstep/views/OverviewActionsView.java
+++ quickstep/src/com/android/quickstep/views/OverviewActionsView.java
@@ -105,6 +105,7 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
     private static final String KEY_RECENTS_CLEAR_ALL = "pref_recents_clear_all";
     private static final String KEY_RECENTS_LENS = "pref_recents_lens";
     private static final String KEY_RECENTS_SHAKE_CLEAR_ALL = "pref_recents_shake_clear_all";
+    private static final String KEY_RECENTS_LOCK = "pref_recents_lock";
 
     private MultiValueAlpha mMultiValueAlpha;
     private Button mSplitButton;
@@ -134,6 +135,7 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
     private boolean mLens;
     private boolean mShakeClearAll;
     private long onClickTime = 0;
+    private boolean mLock;
 
     public OverviewActionsView(Context context) {
         this(context, null);
@@ -149,6 +151,7 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
         mScreenshot = prefs.getBoolean(KEY_RECENTS_SCREENSHOT, true);
         mClearAll = prefs.getBoolean(KEY_RECENTS_CLEAR_ALL, true);
         mLens = prefs.getBoolean(KEY_RECENTS_LENS, false);
+        mLock = prefs.getBoolean(KEY_RECENTS_LOCK, true);
         prefs.registerOnSharedPreferenceChangeListener(this);
         mShakeUtils = new ShakeUtils(context);
         mShakeClearAll = prefs.getBoolean(KEY_RECENTS_SHAKE_CLEAR_ALL, false);
@@ -187,6 +190,11 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
         lens.setOnClickListener(this);
         lens.setVisibility(mLens && Utilities.isGSAEnabled(getContext()) ? VISIBLE : GONE);
         findViewById(R.id.lens_space).setVisibility(mLens && Utilities.isGSAEnabled(getContext()) ? VISIBLE : GONE);
+        
+        View actionLock = findViewById(R.id.action_lock);
+        actionLock.setOnClickListener(this);
+        actionLock.setVisibility(mLock ? VISIBLE : GONE);
+        findViewById(R.id.action_lock_space).setVisibility(mLock ? VISIBLE : GONE);
 
         mSplitButton = findViewById(R.id.action_split);
         mSplitButton.setOnClickListener(this);
@@ -257,7 +265,9 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
         } else if (key.equals(KEY_RECENTS_LENS)) {
             mLens = prefs.getBoolean(KEY_RECENTS_LENS, false);
         } else if (key.equals(KEY_RECENTS_SHAKE_CLEAR_ALL)) {
-            mShakeClearAll = prefs.getBoolean(KEY_RECENTS_SHAKE_CLEAR_ALL, false);
+            mShakeClearAll = prefs.getBoolean(KEY_RECENTS_SHAKE_CLEAR_ALL, true);
+        } else if (key.equals(KEY_RECENTS_LOCK)) {
+            mLock = prefs.getBoolean(KEY_RECENTS_LOCK, false);
         }
         updateVisibilities();
     }
